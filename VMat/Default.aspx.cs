21457dbd67dc60d746ea6534fe17f259e80d5022
Calvin Mlynarczyk <cal.mlynarczyk@gmail.com>
2011-10-28 03:01:08 -0400

    Added notes to update filepaths in the future

--- VMat/Default.aspx.cs
+++ VMat/Default.aspx.cs
@@ -15,7 +15,7 @@ namespace VMat
         protected void Page_Load(object sender, EventArgs e)
         {
             DataSet projectData = new DataSet();
-            projectData.ReadXml(Server.MapPath("Projects.xml"));
+            projectData.ReadXml(Server.MapPath("Projects.xml")); //TODO: Update this in the future to access from external project
             ProjectDisplay.DataSource = projectData.Tables["project"];
             ProjectDisplay.DataBind();
         }
@@ -23,7 +23,7 @@ namespace VMat
         protected void ImageList_Load(object sender, EventArgs e)
         {
             DataSet imagelist = new DataSet();
-            imagelist.ReadXml(Server.MapPath("ImageFiles.xml"));
+            imagelist.ReadXml(Server.MapPath("ImageFiles.xml")); //TODO: Update this in the future to access from external project
             ImageList.DataSource = imagelist.Tables["iso"];
             ImageList.DataTextField = "name";
             ImageList.DataBind();
@@ -32,7 +32,7 @@ namespace VMat
         protected void ProjectList_Load(object sender, EventArgs e)
         {
             DataSet projectList = new DataSet();
-            projectList.ReadXml(Server.MapPath("Projects.xml"));
+            projectList.ReadXml(Server.MapPath("Projects.xml")); //TODO: Update this in the future to access from external project
             ProjectList.DataSource = projectList.Tables["project"];
             ProjectList.DataTextField = "projectname";
             ProjectList.DataBind();
