a5475f1735d7707e670ddcbb530b93143198dfc2
Calvin Mlynarczyk <cal.mlynarczyk@gmail.com>
2011-11-08 23:00:37 -0500

    Added code stubs for incrementer button control scripts

---
+++ VMat/Scripts/formControls.js
@@ -0,0 +1,11 @@
+﻿function incrementUp(obj, val) {
+    var value = val.valueOf;
+    value++;
+    obj.Text = value;
+}
+
+function incrementDown(obj, val) {
+    var value = val.valueOf;
+    value--;
+    obj.Text = value;
+}
