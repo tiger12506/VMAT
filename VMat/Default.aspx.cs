1ceb61ef4f0f1a448183c041bc0616fc35c4341a
Calvin Mlynarczyk <cal.mlynarczyk@gmail.com>
2011-11-10 01:10:03 -0500

    Created DataTable that pulls info from VMware in the machine list UI controller

--- VMat/Default.aspx.cs
+++ VMat/Default.aspx.cs
@@ -7,6 +7,7 @@ using System.Web.UI.WebControls;
 using System.Data.OleDb;
 using System.Data;
 using System.Xml;
+using BackendVMWare;
 
 namespace VMat
 {
@@ -15,8 +16,41 @@ namespace VMat
         protected void Page_Load(object sender, EventArgs e)
         {
             DataSet projectData = new DataSet();
-            projectData.ReadXml(Server.MapPath("Projects.xml")); //TODO: Update this in the future to access from external project
-            ProjectDisplay.DataSource = projectData.Tables["project"];
+            DataTable projectTable = new DataTable();
+            DataColumn machineImagePath = new DataColumn("ImagePath");
+            DataColumn machineStatus = new DataColumn("Status");
+            DataColumn machineHostname = new DataColumn("Hostname");
+            DataColumn machineIPAddress = new DataColumn("IP");
+            DataColumn machineCreated = new DataColumn("Created");
+            DataColumn machineStopped = new DataColumn("Stopped");
+
+            projectTable.Columns.Add(machineImagePath);
+            projectTable.Columns.Add(machineStatus);
+            projectTable.Columns.Add(machineHostname);
+            projectTable.Columns.Add(machineIPAddress);
+            projectTable.Columns.Add(machineCreated);
+            projectTable.Columns.Add(machineStopped);
+
+            VMManager vm_manager = new VMManager();
+
+            foreach (string imageName in vm_manager.getRegisteredVMs())
+            {
+                DataRow machineData = projectTable.NewRow();
+                var vmi = vm_manager.getInfo(imageName);
+
+                machineData[machineImagePath.ColumnName] = vmi.ImagePathName;
+                machineData[machineStatus.ColumnName] = vmi.Status;
+                machineData[machineHostname.ColumnName] = vmi.HostnameWithDomain;
+                machineData[machineIPAddress.ColumnName] = vmi.IP;
+                machineData[machineCreated.ColumnName] = vmi.Created;
+                machineData[machineStopped.ColumnName] = vmi.LastStopped;
+
+                projectTable.Rows.Add(machineData);
+            }
+
+            //projectData.ReadXml(Server.MapPath("Projects.xml")); //TODO: Update this in the future to access from external project
+            //ProjectDisplay.DataSource = projectData.Tables["project"];
+            ProjectDisplay.DataSource = projectTable;
             ProjectDisplay.DataBind();
         }
 
