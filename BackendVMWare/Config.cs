a643957090e55171b32aa05478b3426e19969679
Nathan Mendel <mendelnt@rose-hulman.edu>
2011-11-30 00:42:15 -0500

    Can set hostname and IP, sample in beTest/see blog

---
+++ BackendVMWare/Config.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BackendVMWare
+{
+    class Config
+    {
+
+        public static string getHostVmPath()
+        {
+            return @"C:\Virtual Machines\";
+        }
+        public static string getWebserverVmPath()
+        {
+            return @"\\vmat.csse.rose-hulman.edu\VirtualMachines\";
+        }
+    }
+}
