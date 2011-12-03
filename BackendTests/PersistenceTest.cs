3dff4ef45dc029888291fdeb7efc045d04fff3ed
Calvin Mlynarczyk <cal.mlynarczyk@gmail.com>
2011-12-03 16:51:08 -0500

    Created tests for persistence interface

---
+++ BackendTests/PersistenceTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BackendVMWare;
+
+namespace BackendTests
+{
+    [TestClass]
+    public class PersistenceTest
+    {
+        [TestMethod]
+        public void TestGetValue()
+        {
+            string value = BackendVMWare.Persistence.GetValue("MaxIP");
+            Assert.AreEqual(value, "192.168.1.255");
+        }
+
+        [TestMethod]
+        public void TestWriteValue()
+        {
+            string option = "What";
+            string value = "lol";
+
+            BackendVMWare.Persistence.WriteData(option, value);
+        }
+    }
+}
