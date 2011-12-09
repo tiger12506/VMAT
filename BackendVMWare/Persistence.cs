ada3a4a8e436f104e791d3d7e3ef62763f8213cc
Calvin Mlynarczyk <cal.mlynarczyk@gmail.com>
2011-12-09 13:32:49 -0500

    Comments for the persistence API; fixed bad SQL queries

--- BackendVMWare/Persistence.cs
+++ BackendVMWare/Persistence.cs
@@ -13,6 +13,11 @@ namespace BackendVMWare
         protected const string CONFIGPATH = "C:/Users/Calvin/Documents/VMAT/VMAT/BackendVMWare/HostTest.xls";
         protected const string VMCACHEPATH = "C:/Users/Calvin/Documents/VMAT/VMAT/BackendVMWare/VirtualMachinesTest.xls";
 
+        /// <summary>
+        /// Write the given key-value pair to the host configuration file.
+        /// </summary>
+        /// <param name="option">The key option string</param>
+        /// <param name="value">The value for the associated option</param>
         public static void WriteData(string option, string value)
         {
             DataSet data = new DataSet();
@@ -20,6 +25,12 @@ namespace BackendVMWare
             ConnectDataSource(CONFIGPATH, command, data);
         }
 
+        /// <summary>
+        /// Write the given IP address for the given machine name to the
+        /// static data source.
+        /// </summary>
+        /// <param name="name">The virtual machine's name</param>
+        /// <param name="ip">The desired IP address</param>
         public static void WriteVMIP(string name, string ip)
         {
             DataSet data = new DataSet();
@@ -27,6 +38,12 @@ namespace BackendVMWare
             ConnectDataSource(VMCACHEPATH, command, data);
         }
 
+        /// <summary>
+        /// Return the value associated with the given option in the host
+        /// configuration file.
+        /// </summary>
+        /// <param name="option">The selected option key</param>
+        /// <returns>The value associated with the given option</returns>
         public static string GetValue(string option)
         {
             DataSet data = new DataSet();
@@ -38,15 +55,34 @@ namespace BackendVMWare
             return result;
         }
 
+        /// <summary>
+        /// Return the IP address of the given virtual machine from the
+        /// static data source.
+        /// </summary>
+        /// <param name="name">The target machine's name</param>
+        /// <returns>The IP address of the selected machine</returns>
         public static string GetIP(string name)
         {
             DataSet data = new DataSet();
             string command = "SELECT IP FROM [VirtualMachines$] WHERE Name = '" + name + "'";
-            ConnectDataSource(CONFIGPATH, command, data);
+            ConnectDataSource(VMCACHEPATH, command, data);
 
             string result = data.Tables[0].Rows[0][0].ToString();
 
             return result;
+        }
+
+        /// <summary>
+        /// Return all data stored within the static virtual machine data source.
+        /// </summary>
+        /// <returns>The entire data table of virtual machine information</returns>
+        public static DataSet GetVirtualMachineData()
+        {
+            DataSet data = new DataSet();
+            string command = "SELECT * FROM [VirtualMachines$];";
+            ConnectDataSource(VMCACHEPATH, command, data);
+
+            return data;
         }
 
         private static void ConnectDataSource(string resourceFile, string command, DataSet data)
