6cdd2b5cb4dee55d8106d19ce579edd0b2a3563d
Anthony Sylvain <sylvaiam@rose-hulman.edu>
2011-12-13 04:00:40 -0500

    working

--- VMat/CreateMachine.aspx.cs
+++ VMat/CreateMachine.aspx.cs
@@ -1,4 +1,4 @@
-﻿    using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -19,6 +19,9 @@ namespace VMat
                 ImageList_Load();
                 ProjectList_Load();
             }
+            var bob = System.IO.Directory.GetCurrentDirectory();           
+            Archiving arc = new Archiving();
+            arc.ArchiveFile("C:\\Users\\sylvaiam\\VMAT\\BackendVMWare\\Archiving.cs", "");
         }
 
         private void ImageList_Load()
@@ -26,8 +29,10 @@ namespace VMat
             DataSet imagelist = new DataSet();
             //TODO: Update this in the future to access from external project
             //TODO: Make this information accessible project-wide
-            String conStr = "Provider=Microsoft.Ace.OLEDB.12.0;Data Source=" + Server.MapPath("ImageFiles.xlsx") +
-                ";Extended Properties=Excel 12.0;";
+            String conStr = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + Server.MapPath("ImageFiles.xls") +
+                ";Extended Properties=Excel 8.0;";
+//            System.Configuration.Configuration rootWebConfig = System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("/MyWebSiteRoot");
+//            conStr = rootWebConfig.ConnectionStrings.ConnectionStrings["ImageFiles"].ConnectionString;
             OleDbConnection con = new OleDbConnection(conStr);
             con.Open();
             OleDbCommand cmd = new OleDbCommand("Select * From ImageFiles", con);
