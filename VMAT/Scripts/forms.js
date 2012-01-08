3474a2eedb472a60223e6b0bf5b72b185437efa5
Calvin Mlynarczyk <cal.mlynarczyk@gmail.com>
2012-01-07 23:42:54 -0500

    Fixed the form preview pane; removed obsolete validation scripts

--- VMAT/Scripts/forms.js
+++ VMAT/Scripts/forms.js
@@ -2,13 +2,10 @@
 
 $(document).ready(function () {
     $("form input").change(updatePreviewPane);
+    $("form select").change(updatePreviewPane);
 
     $("#project-menu").change(function () {
         $("#project-menu-field").val($(this).val());
-    });
-
-    $("input.cancel").click(function () {
-        window.location = "/";
     });
 });
 
@@ -16,19 +13,18 @@ $(document).ready(function () {
 // Update the preview display on the 'Create Machine' page with the information
 // currently entered in the form inputs.
 function updatePreviewPane() {
-    var projectNumber = $("#project-menu-field").val();
-    var machineSuffix = $("#machine-suffix").val();
+    var projectNumber = $("#ProjectName").val();
+    var machineSuffix = $("#MachineNameSuffix").val();
     
-    $("#pProject").text("G" + projectNumber);
-    $("#pHostname").text("gapdev.com");
-    $("#pMachinename").text("gapdev" + projectNumber + machineSuffix);
-    $("#pIP").text("192.168.1.1");
+    $(".pProject").text("G" + projectNumber);
+    $(".pHostname").text("gapdev.com");
+    $(".pMachinename").text("gapdev" + projectNumber + machineSuffix);
+    $(".pIP").text("192.168.1.1");
     
-
     try {
-        var imageName = $("#image-menu option:selected").val();
-        $("#pImage").text(imageName);
+        var imageName = $("#BaseImageName option:selected").val();
+        $(".pImage").text(imageName);
     } catch (e) {
-        
+        // Ignore if the field does not exist
     }
 };
