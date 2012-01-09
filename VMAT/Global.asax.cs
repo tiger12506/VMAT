fbe6bafea2616706badf6cc70bec2c97cc05b1b1
Calvin Mlynarczyk <cal.mlynarczyk@gmail.com>
2012-01-08 19:22:34 -0500

    Created connection strings and interface class for the database

--- VMAT/Global.asax.cs
+++ VMAT/Global.asax.cs
@@ -31,6 +31,9 @@ namespace VMAT
 
         protected void Application_Start()
         {
+            //System.Data.Entity.Database.SetInitializer(
+                //new MvcMusicStore.Models.SampleData());
+
             AreaRegistration.RegisterAllAreas();
 
             RegisterGlobalFilters(GlobalFilters.Filters);
---
+++ VMAT/Models/DataEntities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace VMAT.Models
+{
+    public class DataEntities : DbSet
+    {
+        public DbSet<VirtualMachine> VirtualMachines { get; set; }
+        public DbSet<PendingVirtualMachine> PendingVirtualMachines { get; set; }
+        public DbSet<ArchivedVirtualMachine> ArchivedVirtualMachines { get; set; }
+    }
+}
