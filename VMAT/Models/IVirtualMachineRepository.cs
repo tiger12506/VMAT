5fd24500ead93b90208fd00b0389fa74fd2ab49d
Calvin Mlynarczyk <cal.mlynarczyk@gmail.com>
2012-01-21 02:25:36 -0500

    Created stubs for repository pattern to facilitate testing

---
+++ VMAT/Models/IVirtualMachineRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace VMAT.Models
+{
+    public interface IVirtualMachineRepository
+    {
+        public void CreateProject(Project proj);
+        public Project GetProjects();
+        public IEnumerable<VirtualMachine> GetVirtualMachines();
+        public VirtualMachine GetVirtualMachine(string imagePath);
+        public void CreateRegisteredVirtualMachine(RegisteredVirtualMachine vm);
+        public RegisteredVirtualMachine GetRegisteredVirtualMachine(string imagePath);
+        public void CreateArchivedVirtualMachine(ArchivedVirtualMachine vm);
+        public ArchivedVirtualMachine GetArchivedVirtualMachine(string imagePath);
+        public void CreatePendingVirtualMachine(PendingVirtualMachine vm);
+        public PendingVirtualMachine GetPendingVirtualMachine(string imagePath);
+    }
+}
