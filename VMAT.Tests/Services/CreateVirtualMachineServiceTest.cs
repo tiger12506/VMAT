9ce082eaff8fac4b2bea2055ff51bc32bc1f34ad
Calvin Mlynarczyk <cal.mlynarczyk@gmail.com>
2012-01-23 00:16:27 -0500

    Moved old tests to respective test classes and began implementation of mock artifacts and controller tests

---
+++ VMAT.Tests/Services/CreateVirtualMachineServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using VMAT.Models.VMware;
+
+namespace VMAT.Tests.Services
+{
+    [TestClass]
+    public class CreateVirtualMachineServiceTest
+    {
+        //TODO: doesn't test right method
+        [TestMethod]
+        public void TestCreateVM()
+        {
+            // Arrange
+            var imageLocation = @"c:/vm.vmx";
+            var vmm = new MockCreateVirtualMachineService();
+            var mHost = new Mock<IVirtualHost>();
+            var mVM = new Mock<IVirtualMachine>();
+            mHost.Setup(host => host.ConnectToVMWareServer("vmat.reshall.rose-hulman.edu", "Nathan", "Vmat1234"));
+            mHost.Setup(host => host.Open(imageLocation)).Returns(mVM.Object);
+            mVM.Setup(vm => vm.WaitForToolsInGuest());
+            mVM.Setup(vm => vm.LoginInGuest("Administrator", "password"));
+
+            // Act
+            vmm.CreateVM(mHost.Object, imageLocation);
+
+            // Assert
+            mHost.VerifyAll();
+            mVM.VerifyAll();
+            Assert.Inconclusive("Not Implemented");
+        }
+    }
+}
