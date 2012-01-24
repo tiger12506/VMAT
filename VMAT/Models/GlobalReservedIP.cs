479bb48431ea9a5d550adc05cbc480a4d9b4a17d
Calvin Mlynarczyk <cal.mlynarczyk@gmail.com>
2012-01-24 14:53:39 -0500

    Added global dictionary for storing reserved IPs

---
+++ VMAT/Models/GlobalReservedIP.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace VMAT.Models
+{
+    public static class GlobalReservedIP
+    {
+        private static readonly object syncSwitch = new object();
+        private static Dictionary<string, string> reservedIpList;
+
+        public static string GetItemByTag(string tag)
+        {
+            lock (syncSwitch)
+            {
+                if (reservedIpList == null)
+                    reservedIpList = new Dictionary<string, string>();
+
+                return reservedIpList[tag];
+            }
+        }
+
+        public static Dictionary<string, string> GetReservedIPs()
+        {
+            lock (syncSwitch)
+                return reservedIpList;
+        }
+
+        public static void ReserveIP(string imagePathName, string ip)
+        {
+            lock (syncSwitch)
+                reservedIpList[imagePathName] = ip;
+        }
+    }
+}
