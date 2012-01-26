8df4bcc1932baf879d6758a0ad8893da36330069
Calvin Mlynarczyk <cal.mlynarczyk@gmail.com>
2012-01-26 01:14:51 -0500

    Added project collapse button; closes #66

--- VMAT/Scripts/vmManagement.js
+++ VMAT/Scripts/vmManagement.js
@@ -5,6 +5,16 @@
         setStatusTooltips($(this));
     });
 
+    $(".project-display").click(function () {
+        if ($(this).text() === "V") {
+            $(this).text(">");
+        } else {
+            $(this).text("V");
+        }
+
+        $(this).closest(".project").children(".project-machines").slideToggle(300);
+    });
+
     $(".toggle-details").click(function () {
         var $detailsDiv = $(this).closest(".machine-info").children(".details");
 
