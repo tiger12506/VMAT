b9ef276d80c2808c4b1e24de2ee48a30b8dcf8c8
Nathan Mendel <mendelnt@rose-hulman.edu>
2012-02-04 15:50:38 -0500

    added Quartz & some config

---
+++ VMAT/Services/QuartzJobs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Quartz;
+
+namespace VMAT.Services
+{
+    public class MyJob : IJob
+    {
+        #region IJob Members
+
+        public void Execute(JobExecutionContext context)
+        {
+            JobDataMap data = context.MergedJobDataMap;
+
+            string msg = data.GetString("MessageToLog") ?? string.Empty;
+
+        }
+
+        #endregion
+
+    }
+}
